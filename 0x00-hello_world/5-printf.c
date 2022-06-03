8d0374f4824d3629c37815950cf18c0399be5331
kiks2022 <adekoyaakorede@gmail.com>
2022-06-02 17:01:21 -0700

    6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,10 +4,10 @@
  *main - prints the phrase with proper grammer,
  *but the outcome is a piece of art,
  *Return: 0 if exited properly, non-zero otherwise
-*/
+ */
 int main(void)
 {
-printf("with proper grammer, but the outcome is a pieceof art,\n");
+printf("with proper grammer, but the outcome is a piece of art,\n");
 return (0);
 }
 
